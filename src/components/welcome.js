7a7b3b65933b6c18147b4f2ca7262b41b09eefb6
Shyve <shyveee@gmail.com>
2023-09-27 03:15:59 +0200

    test

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { motion } from "framer-motion";
-import emoji from "../../public/dfbe7d64e9cbf7796cead59582156959-sticker.png"
 
 export const Welcome = () => {
   return (
@@ -12,7 +11,7 @@ export const Welcome = () => {
       >
         <div className="content">
           <img
-            src={emoji}
+            src="./portfolio/dfbe7d64e9cbf7796cead59582156959-sticker.png"
             alt="Memoji"
           />
           <div className="texteWelcome">
